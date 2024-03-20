433603e8872490e4d66d712507fd70fef7b16158
YU YANG <williammmmm5316@gmail.com>
2024-03-20 21:28:13 +0900

    0320commit
    divide page into folder , create settingpage demo.

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+import 'package:external_app_launcher/external_app_launcher.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import  'SettingPage.dart';
+
+class MyHomePage extends StatefulWidget {
+  //  constructor
+  const MyHomePage({super.key, required this.title});
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  void _openCamera() async {
+    var openAppResult = await LaunchApp.openApp(
+        androidPackageName: 'com.google.android.GoogleCamera',
+        iosUrlScheme: 'camera://',
+        appStoreLink: 'itms-apps:/apps.apple.com/jp/app/カメラ/id1584216193',
+        openStore: false);
+    // ignore: avoid_print
+    print('openAppResult => $openAppResult ${openAppResult.runtimeType}');
+    // Enter thr package name of the App you want to open and for iOS add the URLscheme to the Info.plist file.
+    // The second arguments decide wether the app redirects PlayStore or AppStore.
+    // For testing purpose you can enter com.instagram.android
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      // button settings
+      /// FloatingActionButtion
+      floatingActionButton:SizedBox(
+        width: 90,
+        height: 90,
+        child: FloatingActionButton(
+          onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) =>const MyHomePage(title: 'home page',)),
+                );
+              },
+          tooltip: 'Increment',
+          child:
+              const Icon(Icons.home, color: Color(0xFFFFFFFF), size: 35),
+        ),
+        ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      /// bottomNavigationBar
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(), // 下部ナビゲーションバーの形状を設定
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            // camera button
+            IconButton(
+              icon: const Icon(Icons.camera_alt,
+                  color: Color(0xFFFFFFFF), size: 30),
+              onPressed: _openCamera,
+            ),
+            // settings button
+            IconButton(
+              icon: const Icon(Icons.settings,
+                  color: Color(0xFFFFFFFF), size: 30),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const SettingPage()),
+                );
+              },
+
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
